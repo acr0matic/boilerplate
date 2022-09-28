53665dc2307f76fc9f75b0a5b005e3d024ba2a1d
acr0matic <work.acr0matic@gmail.com>
2022-09-29 00:22:20 +0300

    feat: добавлен модуль объединения медиа-запросов

--- gulp_tasks/minify_css.js
+++ gulp_tasks/minify_css.js
@@ -2,6 +2,7 @@ const { task, src, dest } = require('gulp');
 const rename = require('gulp-rename');
 const cleanCSS = require('gulp-clean-css');
 const purgecss = require('gulp-purgecss');
+const gcmq = require('gulp-group-css-media-queries');
 
 const paths = require('../gulpfile');
 
@@ -17,6 +18,7 @@ task('minify_css', () => src(paths.styles.temp + paths.styles.out)
       content: ['src/**/*.{html,php}', 'src/**/*.js'],
     }),
   )
+  .pipe(gcmq())
   .pipe(cleanCSS({ level: 2 }))
   .pipe(rename(paths.styles.minify))
   .pipe(dest(paths.styles.dist)));
