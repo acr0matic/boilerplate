15dd51addd1f1b5facbd3c49ff8cb492a4d84c12
acr0matic <work.acr0matic@gmail.com>
2023-02-15 21:52:46 +0300

    fix: подключение базовых скриптов

--- src/scripts/base.js
+++ src/scripts/base.js
@@ -2,3 +2,9 @@ const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
 
 const isMobile = window.matchMedia('(max-width: 576px)').matches;
 const isTablet = window.matchMedia('(max-width: 991px)').matches;
+
+const StyleСlass = {
+  'mobile': {
+    'open': 'mobile-menu--open',
+  },
+}
